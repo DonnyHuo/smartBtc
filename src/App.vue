0ce7bdfbdabdcf35ac496067c2722667f0bd923f
amazingHuo <15710021168@163.com>
2024-09-22 00:36:08 +0800

    update smart contract address

--- src/App.vue
+++ src/App.vue
@@ -253,6 +253,10 @@ body {
   align-items: center;
   justify-content: space-around;
   border-top: 1px solid #eeeeee;
+  padding-bottom: constant(safe-area-inset-bottom);
+  /*兼容 IOS<11.2*/
+  padding-bottom: env(safe-area-inset-bottom);
+  /*兼容 IOS>11.2*/
   a {
     width: 20%;
     font-size: 12px;
