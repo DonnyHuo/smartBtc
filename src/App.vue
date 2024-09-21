3887b34001ded4685625fff3e210375ec68dc420
amazingHuo <15710021168@163.com>
2024-09-22 03:00:55 +0800

    change style

--- src/App.vue
+++ src/App.vue
@@ -226,13 +226,14 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 #app {
   text-align: center;
   color: #2c3e50;
-  height: 100vh;
   max-width: 500px;
+  height: 100vh;
   margin: 0 auto;
+  position: relative;
 }
 
 .nav {
